92bc4a631f504baf89af71bf07ef2b13249f0a6e
Antonino Sirchia <asirchia@gmail.com>
2018-11-07 13:02:10 +0100

    License Added

--- src/it/asirchia/utils/properties/Properties.java
+++ src/it/asirchia/utils/properties/Properties.java
@@ -10,10 +10,22 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 /**
- * Property Manager to retrieve application configuration
- * 
- * @author Antonino Sirchia (sirnino)
- * 
+ *  Property Manager - to retrieve application configuration
+ *  Copyright (C) 2018  - Antonino Sirchia
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  @author Antonino Sirchia (asirchia@gmail.com)
  */
 public class Properties {
 	
