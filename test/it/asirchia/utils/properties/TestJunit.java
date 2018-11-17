3d4268aeaf19a86a4d516334d2026faec9851fb8
asirchia <asirchia@gmail.com>
2018-11-17 19:30:45 +0100

    Test case javadoc

--- test/it/asirchia/utils/properties/TestJunit.java
+++ test/it/asirchia/utils/properties/TestJunit.java
@@ -10,13 +10,30 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
 /**
- * @author asirchia
+ *  TestJunit - to test the PropertyManager project
+ *  Copyright (C) 2018  - Antonino Sirchia
  *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  @author Antonino Sirchia (asirchia@gmail.com)
  */
 class TestJunit {
 	
 	private static final String key = "user.name";
 	
+	/**
+	 * Tests the configuration from file
+	 */
 	@Test
 	void testFromFile() {
 		Optional<String> opt = Properties.fromFile(key);
@@ -27,6 +44,9 @@ class TestJunit {
 		assertTrue("UsernameFromConfigurationFile".equals(opt.get()));
 	}
 	
+	/**
+	 * Tests the configuration from environment variable
+	 */
 	@Test
 	void testFromEnv() {	
 		Optional<String> opt = Properties.fromEnv(key);
@@ -37,6 +57,9 @@ class TestJunit {
 		assertTrue("UsernameFromEnvironment".equals(opt.get()));
 	}
 	
+	/**
+	 * Tests the configuration from Zookeeper server
+	 */
 	@Test
 	void testFromZookeeper() {
 		Optional<String> opt = Properties.fromZookeper(key);
