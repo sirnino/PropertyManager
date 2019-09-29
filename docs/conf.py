1d7afe8082a5783f2ce65b711319322ceb9b0f69
asirchia <asirchia@gmail.com>
2019-09-29 21:14:59 +0200

    Change documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -46,16 +46,16 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'PropertyManager'
-copyright = '2019, Antonino'
-author = 'Antonino'
+project = 'Property Manager'
+copyright = '2019, sirnino'
+author = 'Antonino Sirchia'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = ''
+version = 'latest'
 # The full version, including alpha/beta/rc tags.
 release = ''
 
@@ -64,7 +64,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
